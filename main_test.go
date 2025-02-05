c65325bb3581218642775a6b414fa949b0dbc470
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2025-02-05 18:54:12 +0100

    Correct unit test

--- main_test.go
+++ main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func Test_sayHello(t *testing.T) {
-		name := "Bob"
-		want := "Hello Bob"
+		name := "Alice"
+		want := "Hello Alice"
 
 		if got := sayHello(name); got != want {
 				t.Errorf("hello() = %q, want %q", got, want)
